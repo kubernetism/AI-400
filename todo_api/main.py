7de81aae11b7f524c0c0ba901a074d5862d325f8
SafdarAliShah <programmersafdar@live.com>
2026-01-12 10:22:41 +0500

    env_route working

--- todo_api/main.py
+++ todo_api/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from todo-api!")
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Todo API!", "api_key": os.getenv("API_KEY"), "database_url": os.getenv("DATABASE_URL"), "debug_mode": os.getenv("DEBUG")}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
