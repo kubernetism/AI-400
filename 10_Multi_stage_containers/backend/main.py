2cb57e4a26f7f738079835a3baafc33057264291
SafdarAliShah <programmersafdar@live.com>
2026-02-23 08:33:34 +0500

    feat(frontend): initialize frontend with Next.js, TypeScript, and Tailwind CSS
    - Added package.json for managing dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added SVG assets for UI components
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution

---
+++ 10_Multi_stage_containers/backend/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Multistage Container API",
+    description="An API for the multistage container example",
+    version="0.1.0"
+)
+
+# Define a simple endpoint that returns a greeting message
+
+@app.get("/")
+def read_root():
+    return {"message": "The Container Application is running!"}
+
+# creating todo application with in memory storage
+todos = [
+    {"id": 1, "task": "Buy groceries"},
+    {"id": 2, "task": "Walk the dog"},
+    {"id": 3, "task": "Read a book"},
+    {"id": 4, "task": "Write code"},
+    {"id": 5, "task": "Go to the gym"},
+    {"id": 6, "task": "Cook dinner"},
+] 
+
+#get all todos
+@app.get("/todos")
+def get_todos():
+    return {"todos": todos}
+
+#get a todo by id
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return {"todo": todo}
+    return {"error": "Todo not found"}, 404
+#create a new todo
+@app.post("/todos")
+def create_todo(task: str):
+    new_id = max(todo["id"] for todo in todos) + 1
+    new_todo = {"id": new_id, "task": task}
+    todos.append(new_todo)
+    return {"todo": new_todo}
+#update a todo
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, task: str):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            todo["task"] = task
+            return {"todo": todo}
+    return {"error": "Todo not found"}, 404
+#delete a todo
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo deleted"}
+    return {"error": "Todo not found"}, 404
+
